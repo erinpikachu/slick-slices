e263ad5cd94bb404d72a774205410b1adfb29cff
erinpikachu <e.foster945@gmail.com>
2023-04-03 00:23:55 -0400

    adding pizzas query

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,39 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey I'm the pizzas page!</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query PIZZA_QUERY {
+    pizzas: allSanityPizza {
+      nodes {
+        id
+        name
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+          vegetarian
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
